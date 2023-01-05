8fb15fa34978c7828d61534b7150adc957857a34
linth <linth@aurine.cn>
2023-01-05 10:44:52 +0800

    feat: 基础功能的使用

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 // app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  globalData:{
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
   },
-  globalData: {
-    userInfo: null
+
+  /**
+   * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+   */
+  onLaunch: function () {
+    console.log('life- onLaunch')
+  },
+
+  /**
+   * 当小程序启动，或从后台进入前台显示，会触发 onShow
+   */
+  onShow: function (options) {
+    console.log('life- onShow',options)
+  },
+
+  /**
+   * 当小程序从前台进入后台，会触发 onHide
+   */
+  onHide: function () {
+    console.log('life- onHide')
+  },
+
+  /**
+   * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
+   */
+  onError: function (msg) {
+    
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -11,19 +11,69 @@ Page({
     canIUseGetUserProfile: false,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log("页面加载");
     if (wx.getUserProfile) {
       this.setData({
         canIUseGetUserProfile: true
       })
     }
+    this.requestTestGET()
   },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    console.log("页面初次渲染完成");
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log("页面显示");
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    console.log("页面隐藏");
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    console.log("页面卸载");
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
   getUserProfile(e) {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
     wx.getUserProfile({
@@ -37,12 +87,77 @@ Page({
       }
     })
   },
+
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
     console.log(e)
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+
+  toMain: function () {
+    wx.navigateTo({
+      url: '../main/main',
+    })
+    wx.redirectTo({
+      url: '../main/main',
+    })
+  },
+
+  /**
+   * https get请求测试
+   */
+  requestTestGET() {
+    wx.request({
+      // url: 'https://www.escook.cn/api/get',
+      method: 'GET',
+      data: {
+        name: 'linth',
+        age: '100'
+      },
+      success: (res => {
+        console.log('https- get', res)
+      })
+    })
+  },
+
+  /**
+   * 获取轮播图数据
+   */
+  requestSwiperData(){
+    wx.request({
+      url: 'https://www.escook.cn/slides',
+      method: 'GET',
+      success: (res => {
+        console.log('https- get', res)
+      })
+    })
+  },
+
+  /**
+   *跳转到我的界面(使用编程式导航跳转)
+   */
+  onJumoMine(){
+    wx.switchTab({
+      url: '/pages/mine/mine',
+    })
+  },
+
+  /**
+   * 跳转消息界面(使用编程式导航跳转)
+   */
+  onJumpMessage(){
+    wx.navigateTo({
+      url: '/pages/message/message?name=张三&age=18',
     })
   }
 })
